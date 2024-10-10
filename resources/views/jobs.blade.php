c96feca8ff750c724e94c3d931d7a7303531b77a
aliali <elwakeelali05@gmail.com>
2024-10-10 10:55:38 +0300

    first commit

---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('styles/index.css') }}">
+</head>
+
+<body>
+    <div class="main-container-job">
+        <!-- <div class="header-container-job"></div> -->
+        <div class="container-slider" style="background-image: url('{{ asset('images/image1.png') }}');">
+
+            <div class="filter-slider">
+
+            </div>
+
+        </div>
+        <div class="container-jobs">
+
+            <div class="job-card">
+                <img src="images/image1.png" alt="Company Logo">
+                <h2 class="job-title">مصمم جرافيك</h2>
+                <p class="job-description">
+                    هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل
+                    الخارجي للنص أو شكل توضع الفقرات </p>
+                <div class="job-tags">
+                    <span class="tag">دوام كامل</span>
+                    <span class="tag"> سنة علي الاقل</span>
+                    <span class="tag">خبير</span>
+                </div>
+                <div class="job-actions">
+                    <button class="btn btn-primary">تفاصيل الوظيفة </button>
+
+                </div>
+            </div>
+            <div class="job-card">
+                <img src="images/image1.png" alt="Company Logo">
+                <h2 class="job-title">مصمم جرافيك</h2>
+                <p class="job-description">
+                    هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل
+                    الخارجي للنص أو شكل توضع الفقرات </p>
+                <div class="job-tags">
+                    <span class="tag">دوام كامل</span>
+                    <span class="tag"> سنة علي الاقل</span>
+                    <span class="tag">خبير</span>
+                </div>
+                <div class="job-actions">
+                    <button class="btn btn-primary">تفاصيل الوظيفة </button>
+
+                </div>
+            </div>
+            <div class="job-card">
+                <img src="images/image1.png" alt="Company Logo">
+                <h2 class="job-title">مصمم جرافيك</h2>
+                <p class="job-description">
+                    هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل
+                    الخارجي للنص أو شكل توضع الفقرات </p>
+                <div class="job-tags">
+                    <span class="tag">دوام كامل</span>
+                    <span class="tag"> سنة علي الاقل</span>
+                    <span class="tag">خبير</span>
+                </div>
+                <div class="job-actions">
+                    <button class="btn btn-primary">تفاصيل الوظيفة </button>
+
+                </div>
+            </div>
+            <div class="job-card">
+                <img src="images/image1.png" alt="Company Logo">
+                <h2 class="job-title">مصمم جرافيك</h2>
+                <p class="job-description">
+                    هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل
+                    الخارجي للنص أو شكل توضع الفقرات </p>
+                <div class="job-tags">
+                    <span class="tag">دوام كامل</span>
+                    <span class="tag"> سنة علي الاقل</span>
+                    <span class="tag">خبير</span>
+                </div>
+                <div class="job-actions">
+                    <button class="btn btn-primary">تفاصيل الوظيفة </button>
+
+                </div>
+            </div>
+            <div class="job-card">
+                <img src="images/image1.png" alt="Company Logo">
+                <h2 class="job-title">مصمم جرافيك</h2>
+                <p class="job-description">
+                    هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل
+                    الخارجي للنص أو شكل توضع الفقرات </p>
+                <div class="job-tags">
+                    <span class="tag">دوام كامل</span>
+                    <span class="tag"> سنة علي الاقل</span>
+                    <span class="tag">خبير</span>
+                </div>
+                <div class="job-actions">
+                    <button class="btn btn-primary">تفاصيل الوظيفة </button>
+
+                </div>
+            </div>
+            <div class="job-card">
+                <img src="images/image1.png" alt="Company Logo">
+                <h2 class="job-title">مصمم جرافيك</h2>
+                <p class="job-description">
+                    هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل
+                    الخارجي للنص أو شكل توضع الفقرات </p>
+                <div class="job-tags">
+                    <span class="tag">دوام كامل</span>
+                    <span class="tag"> سنة علي الاقل</span>
+                    <span class="tag">خبير</span>
+                </div>
+                <div class="job-actions">
+                    <button class="btn btn-primary">تفاصيل الوظيفة </button>
+
+                </div>
+            </div>
+       
+        
+
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('styles/index.css') }}">
+</head>
+<body>
+    <div class="main-page">
+        
+    <div class="login-box">
+  <h2>Login</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="" required="">
+      <label>Username</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="" required="">
+      <label>Password</label>
+    </div>
+    <button class="btn-h">دخول</button>
+
+  </form>
+</div>
+    </div>
+</body>
+</html>
